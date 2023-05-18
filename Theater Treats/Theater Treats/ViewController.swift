8c4dde2d8d00947ad0d0d02832a9709019042edb
manan0017 <manankakadiya007@gmail.com>
2023-05-19 02:19:09 +0530

    - Create Project

---
+++ Theater Treats/Theater Treats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Theater Treats
+//
+//  Created by Manan Kakadiya on 19/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
